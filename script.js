746f518bb6357578bcde5776f77833446b96aa88
joancreates <135924201+joancreates@users.noreply.github.com>
2023-08-25 15:28:24 -0700

    Add style change when buttons are click or push
    This helps to recreate a button push animation, whether the button is
    clicked in the ui or their related key is pushed on the keyboard. These
    test are being implemented by sections, as all the logic behind these
    functionalities prove to be effective. They are going to be added to
    all buttons.
    
    The result() function from the equal button, now accepts both keydown
    & click events to be activated.

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const newCalculator = new Calculator();
 const display = document.querySelector("#display");
 const equal = document.querySelector("#equal");
 const opBtns = document.querySelectorAll("[data-op]");
+const btns = document.querySelectorAll(".btn");
 
-equal.addEventListener("click", result);
-window.addEventListener("keyup", opKeyInput);
+btns.forEach((btn) => btn.addEventListener("transitionend", removeTransition));
 opBtns.forEach((btn) => btn.addEventListener("click", opClickInput));
+equal.addEventListener("click", result);
+window.addEventListener("keydown", result);
+window.addEventListener("keydown", opKeyInput);
 
 function opClickInput () {
     if(display.value === "") {
@@ -16,21 +19,28 @@ function opClickInput () {
     }
 }
 
-function numClickInput(e) {
-
-}
-
-function opKeyInput (e) {
+function opKeyInput(e) {
     const key = document.querySelector(`[data-op="${e.key}"]`);
     if(!key) return;
     if(display.value === "") {
         display.value += key.dataset.op;
     } else {
-    display.value += ` ${key.dataset.op} `;
+        display.value += ` ${key.dataset.op} `;
     }
+    key.classList.add("btn-temp");
 }
 
-function result () {
+function removeTransition(e) {
+    if(e.propertyName !== "transform") return;
+    this.classList.remove("btn-temp");
+}
+
+function result (e) {
+    if(e.type === "keydown") {
+        const key = document.querySelector(`[data-eq="${e.key}"]`);
+        if(!key) return;
+        key.classList.add("btn-temp");
+    }
     display.value = newCalculator.calc(display.value);
 }
 
