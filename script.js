8f92fa8f2cb1314dd9a2794f3a45e20728c753e5
joancreates <135924201+joancreates@users.noreply.github.com>
2023-08-26 04:38:30 -0700

    Expand functionality to the rest of buttons
    The testing phase began with only some operator buttons, now that it's
    done. The feature has been expanded to the rest and adapted to fit each
    button unique functionality.

--- script.js
+++ script.js
@@ -3,36 +3,134 @@ const newCalculator = new Calculator();
 const display = document.querySelector("#display");
 const equal = document.querySelector("#equal");
 const opBtns = document.querySelectorAll("[data-op]");
-const btns = document.querySelectorAll(".btn");
+const btns = document.querySelectorAll("[data-btn]");
+const numBtns = document.querySelectorAll("[data-num]");
+const fnBtns = document.querySelectorAll("[data-fn]");
+const numSign = document.querySelector("[data-minus-plus]");
+
 
 btns.forEach((btn) => btn.addEventListener("transitionend", removeTransition));
-opBtns.forEach((btn) => btn.addEventListener("click", opClickInput));
+
 equal.addEventListener("click", result);
 window.addEventListener("keydown", result);
-window.addEventListener("keydown", opKeyInput);
 
-function opClickInput () {
-    if(display.value === "") {
-        display.value += this.dataset.op;
-    } else {
-    display.value += ` ${this.dataset.op} `;
+numBtns.forEach((btn) => btn.addEventListener("click", numInput));
+window.addEventListener("keydown", numInput);
+
+opBtns.forEach((btn) => btn.addEventListener("click", opInput));
+window.addEventListener("keydown", opInput);
+
+fnBtns.forEach((btn) => btn.addEventListener("click", fnInput));
+window.addEventListener("keydown", fnInput);
+
+numSign.addEventListener("click", changeInput);
+window.addEventListener("keydown", changeInput);
+
+function changeInput(e) {
+    let index = display.value.lastIndexOf(" -");
+    let lastValue = display.value[display.value.length - 1];
+ 
+    if(lastValue !== " " && (e.type === "click" || e.key === "F9")) {
+        if(display.value[0] === "-") {
+            display.value = display.value.slice(1);
+        } else if (display.value.includes(" -") && 
+            display.value.slice(index, index + 3) !== " - ") {
+            let lastIndex = display.value.lastIndexOf("-");
+            display.value = display.value.slice(0, lastIndex) 
+            + display.value.slice(lastIndex + 1);
+        } else if(!display.value.includes(" ")) {
+            display.value = "-" + display.value.slice(0);
+        } else {
+            let lastIndex = display.value.lastIndexOf(" ");
+            display.value = display.value.slice(0, lastIndex) 
+            + " -" + display.value.slice(lastIndex + 1);
+        }
+    }
+    if(e.key === "F9") {
+        const key = document.querySelector(`[data-minus-plus]`);
+        key.classList.add("btn-temp");
     }
 }
 
-function opKeyInput(e) {
-    const key = document.querySelector(`[data-op="${e.key}"]`);
-    if(!key) return;
-    if(display.value === "") {
-        display.value += key.dataset.op;
-    } else {
-        display.value += ` ${key.dataset.op} `;
+function fnInput(e) {
+    let lastValue = display.value[display.value.length - 1];
+    
+    if(e.key === "Backspace" || this.dataset.fn === "Backspace") {
+        (lastValue === " ") 
+            ? display.value = display.value.slice(0, -3)
+            : display.value = display.value.slice(0, -1);
     }
-    key.classList.add("btn-temp");
+    // if(e.type === "keydown" && e.key === "Backspace") {
+    //     const key = document.querySelector(`[data-fn="${e.key}"]`);
+    //     if(!key) return;
+    //     (lastValue === " ") 
+    //         ? display.value = display.value.slice(0, -3)
+    //         : display.value = display.value.slice(0, -1);
+    //     key.classList.add("btn-temp-backspace");
+    // }
+
+    if(e.type === "click" && this.dataset.fn === "c") 
+        display.value = display.value = "";
+    if(e.type === "keydown" && e.key === "c") {
+        const key = document.querySelector(`[data-fn="${e.key}"]`);
+        if(!key) return;
+        display.value = display.value = "";
+        key.classList.add("btn-temp");
+    }
+
+    if(e.key === "Backspace" || e.key === "c") {
+        const key = document.querySelector(`[data-fn="${e.key}"]`);
+        (e.key === "Backspace") 
+            ? key.classList.add("btn-temp-backspace")
+            : key.classList.add("btn-temp");
+    }
+}
+
+function numInput(e) {
+    const operators = ["/", "*", "+", "-", "^", "%"];
+    let lastValue = display.value[display.value.length - 1];
+
+    if(e.type === "click") {
+        (lastValue !== " " && mySome(operators)) 
+            ? display.value += ` ${this.dataset.num}`
+            : display.value += this.dataset.num;
+    }
+    if(e.type === "keydown") {
+        const key = document.querySelector(`[data-num="${e.key}"]`);
+        if(!key) return;
+        (lastValue !== " " && mySome(operators)) 
+            ? display.value += ` ${key.dataset.num}`
+            : display.value += key.dataset.num;
+        key.classList.add("btn-temp");
+    }
+
+    function mySome(arr) {
+        return arr.some((num) => {
+            if(lastValue) return lastValue.includes(num)
+        }); 
+    }
+}
+
+function opInput(e) {
+    if(e.type === "click") {
+        (display.value === "") 
+            ? display.value += this.dataset.op
+            : display.value += ` ${this.dataset.op} `;
+    }
+    if(e.type === "keydown") {
+        const key = document.querySelector(`[data-op="${e.key}"]`);
+        if(!key) return;
+        (display.value === "")
+            ? display.value += key.dataset.op
+            : display.value += ` ${key.dataset.op} `;
+        key.classList.add("btn-temp");
+    }
 }
 
 function removeTransition(e) {
     if(e.propertyName !== "transform") return;
     this.classList.remove("btn-temp");
+    this.classList.remove("btn-temp-backspace");
 }
 
 function result (e) {
@@ -44,14 +142,14 @@ function result (e) {
     display.value = newCalculator.calc(display.value);
 }
 
-
 function Calculator() {
     this.methods = {
         "+": (a, b) => a + b,
         "-": (a, b) => a - b,
         "*": (a, b) => a * b,
         "/": (a, b) => a / b,
-        "**": (a, b) => a ** b,
+        "^": (a, b) => a ** b,
+        "%": (a, b) => (a / b) * 100,
     };
 
     this.calc = function (string) {
